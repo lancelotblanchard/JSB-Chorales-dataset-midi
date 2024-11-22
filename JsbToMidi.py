f6da4fdae97db6aa964160ac32af9abd84bf85c4
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2024-11-22 18:46:13 -0500

    Implement transposition

--- JsbToMidi.py
+++ JsbToMidi.py
@@ -7,9 +7,10 @@ from tqdm import tqdm
 import os
 
 if __name__=="__main__":
-    # Retrieve quantization from arguments
+    # Parse the command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("quantization", type=int, help="Quantization of the chorales", choices=[16, 8, 4], default=16)
+    parser.add_argument("-t", "--twelveKeysTranspose", action="store_true", help="Transpose the chorales to all 12 keys")
     args = parser.parse_args()
     quantization = args.quantization
 
@@ -34,41 +35,48 @@ if __name__=="__main__":
     with open(filename) as f:
         chorales = json.load(f)
 
+    transpositions = [0] if not args.twelveKeysTranspose else range(12)
+
     for subset in ["train", "valid", "test"]:
-        for n, chorale in tqdm(enumerate(chorales[subset])):
-            midiFile = mido.MidiFile()
-            quantizationTime = int(midiFile.ticks_per_beat // (quantization / 4))
+        for transposition in transpositions:
+            for n, chorale in tqdm(enumerate(chorales[subset])):
+                midiFile = mido.MidiFile()
+                quantizationTime = int(midiFile.ticks_per_beat // (quantization / 4))
 
-            # Keep track of the last note played for each voice
-            lastChord = [-1] * 4
-            tracks = []
-            times = []
+                # Keep track of the last note played for each voice
+                lastChord = [-1] * 4
+                tracks = []
+                times = []
 
-            # Create a track for each voice
-            for i in range(len(lastChord)):
-                track = mido.MidiTrack()
-                midiFile.tracks.append(track)
-                tracks.append(track)
-                track.append(mido.Message("program_change", program=i, time=0))
-                times.append(0)
+                # Create a track for each voice
+                for i in range(len(lastChord)):
+                    track = mido.MidiTrack()
+                    midiFile.tracks.append(track)
+                    tracks.append(track)
+                    track.append(mido.Message("program_change", program=i, time=0))
+                    times.append(0)
 
-            # Build the MIDI file
-            for i, chord in enumerate(chorale):
-                # Notes off
-                for j, note in enumerate(chord):
-                    # If the note is different from the last one and the last one is not -1, turn it off
-                    if note != lastChord[j] and lastChord[j] != -1:
-                        tracks[j].append(mido.Message("note_off", note=int(lastChord[j]), velocity=64, time=(i*quantizationTime) - times[j] - 1))
-                        times[j] = i*quantizationTime
+                # Build the MIDI file
+                for i, chord in enumerate(chorale):
+                    # Notes off
+                    for j, note in enumerate(chord):
+                        # If the note is different from the last one and the last one is not -1, turn it off
+                        if note != lastChord[j] and lastChord[j] != -1:
+                            tracks[j].append(mido.Message("note_off", note=int(lastChord[j]+transposition), velocity=64, time=(i*quantizationTime) - times[j] - 1))
+                            times[j] = i*quantizationTime
 
-                
-                for j, note in enumerate(chord):
-                    if note != lastChord[j]:
-                        tracks[j].append(mido.Message("note_on", note=int(note), velocity=64, time=0 if i == 0 and j == 0 else 1))
-                        lastChord[j] = note
+                    
+                    for j, note in enumerate(chord):
+                        if note != lastChord[j]:
+                            tracks[j].append(mido.Message("note_on", note=int(note+transposition), velocity=64, time=0 if i == 0 and j == 0 else 1))
+                            lastChord[j] = note
 
-            # Notes off at the end
-            for i in range(len(lastChord)):
-                tracks[i].append(mido.Message("note_off", note=int(lastChord[i]), velocity=64, time=midiFile.ticks_per_beat*4))
+                # Notes off at the end
+                for i in range(len(lastChord)):
+                    tracks[i].append(mido.Message("note_off", note=int(lastChord[i]+transposition), velocity=64, time=midiFile.ticks_per_beat*4))
 
-            midiFile.save(f"midi-outputs/{subset}/chorale_{n:03d}.mid")
+                if args.twelveKeysTranspose:
+                    midiFilename = f"midi-outputs/{subset}/chorale_{subset}_{n:03d}_{transposition:02d}.mid"
+                else:
+                    midiFilename = f"midi-outputs/{subset}/chorale_{subset}_{n:03d}.mid"
+                midiFile.save(midiFilename)
